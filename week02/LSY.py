6e4eefdcc9f48a74371c33da575316b62d033d8a
Esing <55014439+sylee7898@users.noreply.github.com>
2022-09-15 13:58:05 +0900

    Update LSY.py

--- week02/LSY.py
+++ week02/LSY.py
@@ -31,3 +31,47 @@ class Solution(object):
             B_cnt[summ] += 1
         return max(B_cnt)
       
+
+
+#646.py : 아직 안됨
+class Solution(object):
+    def findLongestChain(self, pairs):
+        global cnt, n_max
+        cnt, n_max = 1, 1
+        '''
+        def loop(pairs, stack, a):
+            global cnt, n_max
+            
+            for b in range(a+1, len(pairs)):
+                if pairs[a][1] < pairs[b][0]:
+                    stack.append(pairs[b])
+                    cnt += 1
+                    if b != len(pairs)-1:
+                        c = b
+                        loop(pairs, stack, c)
+                    elif a+1 != len(pairs)-1:
+                        cnt -= 1
+                        
+                if b == len(pairs)-1:
+                    print(stack)
+                    n_max = max(cnt, n_max)
+                    cnt -= 1
+                    return n_max
+            
+            cnt -= 1
+        '''
+        pairs.sort(key=lambda x: x[0])
+        i = 0
+        while(i < len(pairs)-1):
+            if pairs[i][0] == pairs[i+1][0]:
+                if (pairs[i][1]-pairs[i][0]) < (pairs[i+1][1]-pairs[i+1][0]):
+                    del pairs[i+1]
+                else:
+                    del pairs[i]
+            i += 1
+        print("pairs : ", pairs)
+        for i in range(len(pairs)-1):
+            cnt = 1
+            stack = [pairs[i]]
+            loop(pairs, stack, i)
+        return n_max
