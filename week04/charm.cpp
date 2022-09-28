242420735406e8cda62664654919f7a57e0e9a6a
dlacofla <90876561+dlacofla@users.noreply.github.com>
2022-09-28 13:33:27 +0900

    2186, 1145

--- week04/charm.cpp
+++ week04/charm.cpp
@@ -28,3 +28,31 @@ public:
     }
 };
 
+//1145. Binary Tree Coloring Game
+//Runtime : 7 ms, faster than 50.41% 
+//Memory : 10.7 MB, less than 64.55%
+class Solution {
+public:
+    int l_cnt = 0;
+    int r_cnt = 0;
+    
+    void dfs(TreeNode* node, int& cnt, int& x, bool isFind){
+        if (node == NULL) return;
+        if (isFind) cnt++;
+        if (node->val == x) {
+            isFind = true;
+            dfs(node->left, l_cnt, x, isFind);
+            dfs(node->right, r_cnt, x, isFind);
+        }
+        dfs(node->left, cnt, x, isFind);
+        dfs(node->right, cnt, x, isFind);
+    }
+    
+    bool btreeGameWinningMove(TreeNode* root, int n, int x) { //같은 깊이의 다른 노드 선택, 무조건 왼쪽으로
+        int cnt = 0;
+        dfs(root, cnt, x, false);
+        int m_cnt = max({l_cnt, r_cnt, (n-(l_cnt+r_cnt+1))});
+        if (m_cnt > n-m_cnt) return true;
+        return false;
+    }
+};
