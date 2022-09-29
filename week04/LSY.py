4cfc19fb1b722956dc8e8ee71c46a5ef6d80e869
Esing <55014439+sylee7898@users.noreply.github.com>
2022-09-29 12:05:10 +0900

    help me~~~~~

--- week04/LSY.py
+++ week04/LSY.py
@@ -7,4 +7,23 @@ class Solution(object):
         return sum(st.values()) + sum(ts.values())
       
 # 1145
-#
+# nonononononononononononononono 노이해 알려죠오오오오
+class Solution(object):
+    def btreeGameWinningMove(self, root, n, x):
+        def count(root, x):
+            if(root is None):
+                return 0
+            l = count(root.left, x)
+            r = count(root.right, x)
+            
+            if (root.val == x):
+                leftCnt = l
+                rightCnt = r
+                
+            return 1 + l + r
+        
+        leftCnt, rightCnt = 0, 0
+
+        x_cnt = count(root, x)
+        y_cnt = n - x_cnt - 1
+        return (y_cnt) > n / 2
